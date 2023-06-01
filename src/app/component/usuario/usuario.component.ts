5c2d9da20a3894c955ae97875426e4c969cc3315
tibotafogo <tibotafog22@gmail.com>
2023-06-01 09:59:28 -0300

    services

--- src/app/component/usuario/usuario.component.ts
+++ src/app/component/usuario/usuario.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/service/usuario.service';
+import { Usuario } from 'src/app/Usuario';
 
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
   styleUrls: ['./usuario.component.css']
 })
-export class UsuarioComponent {
-public dados = [
-  {id:1, nome:"Heitor", idade: 25, sexo:"M", telefone:99999999},
-  {id:2, nome:"Hugo", idade: 42, sexo:"M", telefone:888888888},
-  {id:3, nome:"Henrique", idade: 35, sexo:"M", telefone:77777777},
-  {id:4, nome:"Manuela", idade: 25, sexo:"F", telefone:66666666}
+export class UsuarioComponent implements OnInit {
 
-]
+  constructor(private usuarioService:UsuarioService){}
+
+  ngOnInit(): void {}
+
+  usuario: Usuario[] = [
+    {id: 1, nome: "Hugo", idade:26},
+    {id: 2, nome: "Manu", idade:7}
+  ]
+
+  
+
+
+
 }
